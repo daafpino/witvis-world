8008f75b986f29543095c147867b08c9daf8e5f7
David Pino <davidpino@Davids-MBP-2.fritz.box>
2025-05-19 12:13:17 +0200

    Add console logs to debug Supabase env vars

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+console.log('REACT_APP_SUPABASE_URL:', process.env.REACT_APP_SUPABASE_URL);
+console.log('REACT_APP_SUPABASE_KEY:', process.env.REACT_APP_SUPABASE_KEY);
 
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
